71ea992d430f4fc2f1dcb790be81dddc21ab17e9
Nikolai Motaev <n.motaev@itransition.com>
2018-03-18 09:54:57 +0300

    config was added

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,4 +1,28 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+function getMnemonic() {
+    var jsonContent = require('fs').readFileSync("./secrets.json", "utf8").trim();
+
+    return JSON.parse(jsonContent);
+}
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        main: {
+            network_id: 1,
+            provider: new HDWalletProvider(getMnemonic().mnemonic, 'https://mainnet.infura.io/'),
+            gas: 4700000,
+            gasPrice: 1000000000
+        },
+        rinkeby:{
+            network_id:4,
+            provider: new HDWalletProvider(getMnemonic().mnemonic, 'https://rinkeby.infura.io/'),
+            gas: 4700000,
+            gasPrice: 10000000000
+        },
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
+    }
 };
