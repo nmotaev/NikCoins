ac12a6146c771ee890fde26019eca8bdc11f44f9
Nikolai Motaev <n.motaev@itransition.com>
2018-03-19 16:44:13 +0300

    exchange was updated

--- truffle/contracts/Exchange.sol
+++ truffle/contracts/Exchange.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.4.18;
 import "./ExchangeInterface.sol";
 import "./helper/Owned.sol";
 import "./TokenExchangeManager.sol";
+import "./NikToken.sol";
 
 contract Exchange is ExchangeInterface, Owned {
 
     TokenExchangeManager internal tokenManager;
+
     uint internal sellRate = 1;
     uint internal buyRate = 1;
 
     function sellTokens(uint amount) public returns (bool success) {
         address to = msg.sender;
         uint amountWithRate = amount * sellRate;
-        tokenManager.token().transfer(to, amountWithRate);
+        getTokenHandler().transfer(to, amount);
 
         return true;
     }
@@ -21,9 +23,13 @@ contract Exchange is ExchangeInterface, Owned {
     function buyTokens(uint amount) public returns (bool success) {
         address from = msg.sender;
         uint amountWithRate = amount * buyRate;
-        tokenManager.token().transferFrom(from, address(this), amountWithRate);
+        getTokenHandler().transferFrom(from, address(this), amountWithRate);
 
         return true;
+    }
+
+    function getTokenHandler() private returns (NikToken) {
+        return NikToken(tokenManager.getTokenHandler());
     }
 
     function setTokenManager(address _tokenManager) public onlyOwner returns (bool success) {
--- truffle/migrations/5_Exchange_migration.js
+++ truffle/migrations/5_Exchange_migration.js
@@ -1,4 +1,4 @@
-var Contract = artifacts.require("./TokenExchangeManager.sol");
+var Contract = artifacts.require("./Exchange.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(Contract);
