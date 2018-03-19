cb0ed0bbb3bbd416f42ea26df921eba6cb835607
Nikolai Motaev <n.motaev@itransition.com>
2018-03-19 15:26:41 +0300

    TokenExchangeManager was updated

--- truffle/contracts/Exchange.sol
+++ truffle/contracts/Exchange.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
 import "./ExchangeInterface.sol";
 import "./helper/Owned.sol";
--- truffle/contracts/TokenExchangeManager.sol
+++ truffle/contracts/TokenExchangeManager.sol
@@ -1,16 +1,27 @@
 pragma solidity ^0.4.18;
 
 import "./helper/Owned.sol";
-import "./NikToken.sol";
-import "./Exchange.sol";
 
 contract TokenExchangeManager is Owned {
 
-    NikToken public token;
-    Exchange public exchange;
+    address internal tokenAddress;
+    address internal exchangeAddress;
     bool public active = false;
 
+    modifier mustBeActive {
+        require(active);
+        _;
+    }
+
     event ErrorDebug(string message);
+
+    function getTokenHandler() public mustBeActive view returns (address tokenAddress) {
+        return tokenAddress;
+    }
+
+    function getExchangeHandler() public mustBeActive view returns (address exchangeAddress) {
+        return exchangeAddress;
+    }
 
     function deactivate() public onlyOwner returns (bool success) {
         if (active) {
@@ -28,14 +39,14 @@ contract TokenExchangeManager is Owned {
         return true;
     }
 
-    function init (address tokenAddress, address exchangeAddress) public onlyOwner returns (bool) {
+    function init (address _tokenAddress, address _exchangeAddress) public onlyOwner returns (bool) {
         if (active) {
             ErrorDebug("At first you must deactivate this manager.");
 
             return false;
         }
-        token = NikToken(tokenAddress);
-        exchange = Exchange(exchangeAddress);
+        tokenAddress = _tokenAddress;
+        exchangeAddress = _exchangeAddress;
         activate();
 
         return true;
